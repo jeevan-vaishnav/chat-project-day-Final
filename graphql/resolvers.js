2e1a2af8123ca3904dd8ffbfe73afe4083f4245b
Jeevan0109 <jeevanvaishnavv@gmail.com>
2021-11-07 00:39:35 +0530

    Initial commit

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,21 +1,88 @@
 const bycrpt = require("bcryptjs");
-const { UserInputError } = require("apollo-server");
+const { UserInputError, AuthenticationError } = require("apollo-server");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/env.json");
 const { User } = require("../models");
+const { Op } = require("sequelize");
 
 // A map of functions which return data for the schema.
 module.exports = {
   Query: {
-    getUsers: async () => {
+    getUsers: async (_, __, context) => {
       try {
-        const users = await User.findAll();
+        let user;
+        if (context.req && context.req.headers.authorization) {
+          const token = context.req.headers.authorization.split("Bearer ")[1];
+          jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+            if (err) {
+              throw new AuthenticationError("Unauthenticated");
+            }
+            user = decodedToken;
+          });
+        }
+
+        const users = await User.findAll({
+          where: { username: { [Op.ne]: user.username } },
+        });
         return users;
       } catch (error) {
+        throw error;
+      }
+    }, //end getUser Query
+
+    login: async (_, args) => {
+      const { username, password } = args;
+      let errors = {};
+
+      try {
+        //user and password must not empty
+        if (username.trim() === "")
+          errors.username = "username must not be empty";
+        if (password === "") errors.password = "password must not be empty";
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError("Bad Input", { errors });
+        }
+
+        const user = await User.findOne({
+          where: { username },
+        });
+
+        //if user NA
+        if (!user) {
+          errors.username = "user not found";
+          throw new UserInputError("user not found ", { errors });
+        }
+
+        const correctPassword = await bycrpt.compare(password, user.password);
+
+        if (!correctPassword) {
+          errors.password = "password is incorrect";
+          throw new AuthenticationError("password is incorrect", { errors });
+        }
+
+        //adding token
+        const token = jwt.sign(
+          {
+            username,
+          },
+          JWT_SECRET,
+          { expiresIn: 60 * 60 }
+        );
+
+        return {
+          ...user.toJSON(),
+          createdAt: user.createdAt.toISOString(),
+          token,
+        };
+      } catch (error) {
         console.log(error);
+        throw error;
       }
     },
   },
 
-  //mutation
+  //mutation register
   Mutation: {
     register: async (_, args) => {
       let { username, email, password, confirmPassword } = args;
